4e230e88925316cc764bc4fb2f6c9bd572915deb
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-10-22 21:09:36 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-# API endpoint URL
-url = "http://localhost:8000/predict"
+ip = "3.10.52.202" # AWS ECS public URL 
 
-# Previous date: "26-10-2012"
+# API endpoint URL 8000 (fastapi) | 80 (docker)
+# url = "http://localhost:80/predict_sales"
+url = f"http://{ip}/predict_sales"
 
 # Sample input data
 input_data = {
@@ -29,3 +30,30 @@ if response.status_code == 200:
 else:
     print(f"Error: {response.status_code}")
     print(response.text)
+
+# API endpoint URL for forecast_sales
+# url_forecast = "http://localhost:80/forecast_sales"
+url_forecast = f"http://{ip}/forecast_sales"
+
+params_forecast = {
+    "store_id": 1,
+    "steps": 5
+}
+
+response_forecast = requests.post(url_forecast, params=params_forecast)
+
+if response_forecast.status_code == 200:
+    result_forecast = response_forecast.json()
+    predictions = result_forecast.get("predictions", [])
+
+    if predictions:
+        print(f"Sales forecast for the next {params_forecast['steps']} weeks:")
+        for prediction in predictions:
+            date = prediction["Date"]
+            sales = prediction["Sales"]
+            print(f"Date: {date}, Predicted Sales: ${sales}")
+    else:
+        print("No predictions found.")
+else:
+    print(f"Error: {response_forecast.status_code}")
+    print(response_forecast.text)
